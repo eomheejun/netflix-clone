1c4eb1cd09c86b4ac7149b7c3c2f92ed9f1a481b
Unknown <gmlwns7430@naver.com>
2019-08-25 10:59:05 +0900

    Container default setting#3

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import SearchPresenter from "./SearchPresenter";
+import { tvApi, moviesApi } from '../../api';
 
 export default class extends React.Component {
     state =  {
         tvResults: null,
         movieResults: null,
+        searchTerm: "",
         error: null,
         loading: true
+
     };
+
+    async componentDidMount() {
+        const {searchTerm} = this.state;
+        try{
+            const {
+                data: { results: tvResults}
+            } = await tvApi.search(searchTerm);
+            const {
+                data: { results: movieResults}
+            } = await moviesApi.search(searchTerm);
+            this.setState({
+                movieResults,
+                tvResults
+            });
+        }catch{
+            this.setState({
+                error: "Can't find movies information"
+            });
+        }finally{
+            this.setState({
+                loading: false
+            });
+        }
+    }
 
     render() {
         const{tvResults, movieResults, error, loading}
--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TVPresenter from "./TVPresenter";
+import {tvApi} from "../../api";
 
 export default class extends React.Component {
     state = {
@@ -10,6 +11,34 @@ export default class extends React.Component {
         loading: true
 
     };
+
+    async componentDidMount() {
+        try{
+            const {
+                data: { result: topRated}
+            } = await tvApi.topRated();
+            const {
+                data: { result: popular}
+            } = await tvApi.popular();
+            const {
+                data: { result: airingToday}
+            } = await tvApi.airingToday();
+            this.setState({
+                topRated,
+                popular,
+                airingToday
+            });
+            
+        }catch{
+            this.setState({
+                error: "Can't find movies information"
+            });
+        }finally{
+            this.setState({
+                loading: false
+            });
+        }
+    }
 
     render() {
         const{topRated, upcoming, popular, error, loading} = this.state;
