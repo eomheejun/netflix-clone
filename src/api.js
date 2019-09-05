5248947b54aabcb1bffbeb9f33995d6eaa16dbe9
Unknown <gmlwns7430@naver.com>
2019-09-05 21:21:15 +0900

    Add Poster

--- src/api.js
+++ src/api.js
@@ -49,7 +49,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api.themoviedb.org/31/',
+    baseURL: 'https://api.themoviedb.org/3/',
     params: {
         language: 'en-US'
     }
